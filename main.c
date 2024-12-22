81ae688faac51770b94a610e192fec2bfb37813c
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-12-21 22:40:54 -0700

    [Initial Commit]

--- main.c
+++ main.c
@@ -10,103 +10,316 @@
 #include <string.h>
 #include <stdbool.h>
 
+// json 
+#include <json/json.h>
+
 // Stream processor
 #include <stream_processor/stream_processor.h>
 
-// Function declarations
-/**!
- * Return the size of a file IF buffer == 0 ELSE read a file into buffer
+struct list_processor_symbol_s;
+typedef struct list_processor_symbol_s list_processor_symbol;
+
+struct list_processor_cell_s;
+typedef struct list_processor_cell_s list_processor_cell;
+
+struct list_processor_s;
+typedef struct list_processor_s list_processor;
+
+struct list_processor_cell_s
+{
+
+    int _symbol;
+
+    union
+    {
+        list_processor_cell   *p_cons;
+        list_processor_symbol *p_symbol;
+    };
+
+    list_processor_cell *p_cdr;
+};
+
+struct list_processor_s
+{
+    size_t node_quantity;
+};
+
+/** !
+ * Parse command line arguments
  * 
- * @param path path to the file
- * @param buffer buffer
- * @param binary_mode "wb" IF true ELSE "w"
+ * @param argc               the argc parameter of the entry point
+ * @param argv               the argv parameter of the entry point
+
  * 
- * @return 1 on success, 0 on error
+ * @return void on success, program abort on failure
  */
-size_t load_file ( const char *path, void *buffer, bool binary_mode );
+void parse_command_line_arguments ( int argc, const char *argv[], size_t *p_memory_size );
+
+// Data
+struct
+{
+    size_t used;
+    size_t memory_size;
+    char *p_memory;
+} cell_bump;
+
+list_processor_cell *cell_alloc ( void );
 
 // Entry point
 int main ( int argc, const char *argv[] )
 {
     
     // Initialized data
-    stream_processor *p_stream_processor = (void *) 0;
-    char _file_buffer[4096] = { 0 };
-    json_value *p_value = (void *) 0;
+    size_t memory_pool_size = 65536;
+
+    // Parse command line arguments
+    parse_command_line_arguments(argc, argv, &memory_pool_size);
+
+    if ( memory_pool_size == -1 ) return EXIT_FAILURE;
+
+    //log_info("[eddy] [bump] Allocated %d bytes\n", memory_pool_size);
+
+    cell_bump.used = 0;
+    cell_bump.memory_size = memory_pool_size;
+    cell_bump.p_memory = realloc(0, memory_pool_size);
+
+    while ( !feof(stdin) )
+    {
+
+        // Initialized data
+        char _text[256] = { 0 };
+        int t = 0;
+        size_t parsed = 0;
+        size_t token_len = 0;
+        char _token[256] = { 0 };
+        char *p_token = _text;
+        char c = 0;
+
+        // Prompt
+        fprintf(stderr, "🐶 ");
+
+        // Read
+        c = getchar();
+        
+        // 
+        while ( !feof(stdin) )
+        {
+            
+            // Initialized data
+            int l_t = t;
+            
+
+            //
+            switch (c)
+            {
+                case '(':
+                case ')':
+                    printf("{\"separator\":\"%c\"}", c);
+                    putchar('\n');
+                    
+                    break;
+                case '\n':
+                case ' ':
+
+                    break;
+                    
+                default:            
+                    printf("{\"identifier\":\"");
+                    
+                    do  { putchar(c); c = getchar(); } 
+                    while
+                    (
+                        c != ' ' &&
+                        c != '(' &&
+                        c != ')' 
+                    );
+                    ungetc(c, stdin);
+                    printf("\"}\n");
+                    break;
+            }
+            
+            // 
+            c = getchar();
+
+        }
+    }
     
-    // Load a stream processor file
-    load_file("resources/simple.json", &_file_buffer, false);
-
-    // Parse the file
-    json_value_parse(&_file_buffer, 0, &p_value);
-
-    // Construct the stream processor
-    stream_processor_construct(&p_stream_processor, p_value);
-
-    // Get stream processor info
-    stream_processor_info(p_stream_processor);
-
-    // Start the stream processor
-    stream_processor_start(p_stream_processor);
-
-    // Wait for the stream processor to stop
-    //stream_processor_wait_idle(p_stream_processor);
+    cell_bump.p_memory = realloc(cell_bump.p_memory, 0);
 
     // Success
     return EXIT_SUCCESS;
 }
 
-size_t load_file ( const char *path, void *buffer, bool binary_mode )
+list_processor_cell *cell_alloc ( void )
 {
 
-    // Argument checking 
-    if ( path == 0 ) goto no_path;
+    if ( cell_bump.used > cell_bump.memory_size + sizeof(list_processor_cell) ) cell_bump.used = 0;
+ 
 
-    // Initialized data
-    size_t  ret = 0;
-    FILE   *f   = fopen(path, (binary_mode) ? "rb" : "r");
-    
-    // Check if file is valid
-    if ( f == NULL ) goto invalid_file;
+    list_processor_cell *p_cell = (list_processor_cell *) cell_bump.p_memory[cell_bump.used];
 
-    // Find file size and prep for read
-    fseek(f, 0, SEEK_END);
-    ret = (size_t) ftell(f);
-    fseek(f, 0, SEEK_SET);
-    
-    // Read to data
-    if ( buffer ) 
-        ret = fread(buffer, 1, ret, f);
 
-    // The file is no longer needed
-    fclose(f);
     
+    return p_cell;
+}
+
+void parse_command_line_arguments ( int argc, const char *argv[], size_t *p_memory_size )
+{
+
+    // If no command line arguments are supplied, run all the examples
+    if ( argc == 1 ) return;
+    size_t mem = 0;
+    size_t scale = 1; 
+    char unit = 0;
+    
+    if ( strcmp(argv[1], "memory") == 0 )
+        switch (sscanf(argv[2], "%d%c", &mem, &unit)) {case 2:
+            switch ( unit )
+            {
+                case 'b':
+                case 'B':
+                    scale = 1;
+                    break;
+                case 'k':
+                case 'K':
+                    scale = 1024;
+                    break;
+                case 'm':
+                case 'M':
+                    scale = 1024*1024;
+                    break;
+                case 'g':
+                case 'G':
+                    scale = 1024*1024*1024;
+                    break;
+                default:
+                    break;
+            }
+            break;default:break;}
+
+    // Default
+    else goto invalid_arguments;
+    
+    *p_memory_size = mem * scale;
+
     // Success
-    return ret;
-
+    return;
+    
     // Error handling
     {
 
         // Argument errors
         {
-            no_path:
-                #ifndef NDEBUG
-                    log_error("[json] Null path provided to function \"%s\n", __FUNCTION__);
-                #endif
+            invalid_arguments:
+                
+                // Print a usage message to standard out
+                //print_usage(argv[0]);
 
-                // Error
-                return 0;
-        }
-
-        // File errors
-        {
-            invalid_file:
-                #ifndef NDEBUG
-                    printf("[Standard library] Failed to load file \"%s\". %s\n",path, strerror(errno));
-                #endif
-
-                // Error
-                return 0;
+                // Abort
+                exit(EXIT_FAILURE);
         }
     }
 }
+
+// size_t load_file ( const char *path, void *buffer, bool binary_mode )
+// {
+
+//     // Argument checking 
+//     i
+// // Function declarations
+// /**!
+//  * Return the size of a file IF buffer == 0 ELSE read a file into buffer
+//  * 
+//  * @param path path to the file
+//  * @param buffer buffer
+//  * @param binary_mode "wb" IF true ELSE "w"
+//  * 
+//  * @return 1 on success, 0 on error
+//  */
+// size_t load_file ( const char *path, void *buffer, bool binary_mode );
+
+// // Entry point
+// int main ( int argc, const char *argv[] )
+// {
+    
+//     // Initialized data
+//     stream_processor *p_stream_processor = (void *) 0;
+//     char _file_buffer[4096] = { 0 };
+//     json_value *p_value = (void *) 0;
+    
+//     // Load a stream processor file
+//     load_file("resources/simple.json", &_file_buffer, false);
+
+//     // Parse the file
+//     json_value_parse(_file_buffer, 0, &p_value);
+
+//     // Construct the stream processor
+//     //stream_processor_construct(&p_stream_processor, p_value);
+
+//     // Get stream processor info
+//     //stream_processor_info(p_stream_processor);
+
+//     // Start the stream processor
+//     //stream_processor_start(p_stream_processor);
+
+//     // Wait for the stream processor to stop
+//     //stream_processor_wait_idle(p_stream_processor);
+
+//     // Success
+//     return EXIT_SUCCESS;
+// }
+
+// size_t load_file ( const char *path, void *buffer, bool binary_mode )
+// {
+
+//     // Argument checking 
+//     if ( path == 0 ) goto no_path;
+
+//     // Initialized data
+//     size_t  ret = 0;
+//     FILE   *f   = fopen(path, (binary_mode) ? "rb" : "r");
+    
+//     // Check if file is valid
+//     if ( f == NULL ) goto invalid_file;
+
+//     // Find file size and prep for read
+//     fseek(f, 0, SEEK_END);
+//     ret = (size_t) ftell(f);
+//     fseek(f, 0, SEEK_SET);
+    
+//     // Read to data
+//     if ( buffer ) 
+//         ret = fread(buffer, 1, ret, f);
+
+//     // The file is no longer needed
+//     fclose(f);
+    
+//     // Success
+//     return ret;
+
+//     // Error handling
+//     {
+
+//         // Argument errors
+//         {
+//             no_path:
+//                 #ifndef NDEBUG
+//                     log_error("[json] Null path provided to function \"%s\n", __FUNCTION__);
+//                 #endif
+
+//                 // Error
+//                 return 0;
+//         }
+
+//         // File errors
+//         {
+//             invalid_file:
+//                 #ifndef NDEBUG
+//                     printf("[Standard library] Failed to load file \"%s\". %s\n",path, strerror(errno));
+//                 #endif
+
+//                 // Error
+//                 return 0;
+//         }
+//     }
+// }
--- stream_processor.c
+++ stream_processor.c
@@ -29,7 +29,6 @@ struct stream_processor_node_s
             char _path[FILENAME_MAX];
             stream_processor_node *p_source;
         } output_stream;
-        
 
         struct 
         {
@@ -51,7 +50,7 @@ struct stream_processor_s
 };
 
 // Function declarations
-int stream_processor_node_constructor         ( stream_processor_node **pp_node, const char *p_name, json_value *p_value );
+//int stream_processor_node_constructor         ( stream_processor_node **pp_node, const char *p_name, json_value *p_value );
 int stream_processor_node_input_stream_constructor    ( stream_processor_node **pp_node, json_value *p_value );
 int stream_processor_node_output_stream_constructor    ( stream_processor_node **pp_node, json_value *p_value );
 int stream_processor_node_program_constructor ( stream_processor_node **pp_node, json_value *p_value );
@@ -66,17 +65,17 @@ struct
     [0] =
     {
         ._name = "input stream",
-        .pfn_constructor = (fn_stream_processor_node_constructor* ) stream_processor_node_input_stream_constructor,
+        //.pfn_constructor = (fn_stream_processor_node_constructor* ) stream_processor_node_input_stream_constructor,
     },
     [1] =
     {
         ._name = "output stream",
-        .pfn_constructor = (fn_stream_processor_node_constructor* ) stream_processor_node_output_stream_constructor,
+        //.pfn_constructor = (fn_stream_processor_node_constructor* ) stream_processor_node_output_stream_constructor,
     },
     [2] = 
     {
         ._name = "program",
-        .pfn_constructor = (fn_stream_processor_node_constructor* ) stream_processor_node_program_constructor
+        //.pfn_constructor = (fn_stream_processor_node_constructor* ) stream_processor_node_program_constructor
     }
 };
 dict *p_stream_processor_node_constructors = (void *) 0;
@@ -143,11 +142,11 @@ int stream_processor_construct ( stream_processor **const pp_stream_processor, c
     // Construct each node
     for (size_t i = 0; i < p_stream_processor->p_node_graph->node_quantity; i++)
     {
-        char *p_name = &p_stream_processor->p_node_graph->_p_nodes[i]->_name;
+        char *p_name = p_stream_processor->p_node_graph->_p_nodes[i]->_name;
 
-        stream_processor_node_constructor(&p_stream_processor->_p_nodes[i], p_name, p_stream_processor->p_node_graph->_p_nodes[i]->value);
+        //stream_processor_node_constructor(&p_stream_processor->_p_nodes[i], p_name, p_stream_processor->p_node_graph->_p_nodes[i]->value);
 
-        dict_add(p_stream_processor->p_nodes, &p_stream_processor->_p_nodes[i]->_name, p_stream_processor->_p_nodes[i]);
+        dict_add(p_stream_processor->p_nodes, p_stream_processor->_p_nodes[i]->_name, p_stream_processor->_p_nodes[i]);
     }
 
     // Construct each connection
@@ -173,202 +172,6 @@ int stream_processor_info ( stream_processor *p_stream_processor )
 
     // Print each node in the graph
     node_graph_print(p_stream_processor->p_node_graph);
-
-    // Success
-    return 1;
-}
-
-void *stream_processor_input_stream_thread ( stream_processor_node *p_stream_processor_node )
-{
-
-    static int d = 0;
-
-    while (!feof(p_stream_processor_node->input_stream.p_f))
-    {
-
-        monitor_wait(&p_stream_processor_node->_read_monitor);
-
-        printf("read %d\n", d);
-        d++;
-        
-        fread(p_stream_processor_node->_buffer, sizeof(p_stream_processor_node->_buffer), 1, p_stream_processor_node->input_stream.p_f);
-
-        monitor_notify(p_stream_processor_node->p_write_monitor);
-    };
-
-    p_stream_processor_node->input_stream.eof = true;
-
-    // Success
-    return (void *) 1;
-}
-
-void *stream_processor_output_stream_thread ( stream_processor_node *p_stream_processor_node )
-{
-
-
-    static int d = 0;
-
-    while (1)
-    {
-
-        monitor_wait(&p_stream_processor_node->_read_monitor);
-
-        printf("write %d\n", d);
-        d++;
-
-        fwrite(p_stream_processor_node->output_stream.p_source->_buffer, sizeof(p_stream_processor_node->output_stream.p_source->_buffer), 1, p_stream_processor_node->output_stream.p_f);
-        
-        monitor_notify(p_stream_processor_node->p_write_monitor);
-    };
-
-    // Success
-    return (void *) 1;
-}
-
-int stream_processor_start ( stream_processor *p_stream_processor )
-{
-
-    monitor_create(&p_stream_processor->_monitor);
-
-    p_stream_processor->_p_sources[0]->p_write_monitor = &p_stream_processor->_p_drains[0]->_read_monitor;
-    p_stream_processor->_p_drains[0]->p_write_monitor = &p_stream_processor->_p_sources[0]->_read_monitor;
-    p_stream_processor->_p_drains[0]->output_stream.p_source = p_stream_processor->_p_sources[0];
-
-    for (size_t i = 0; i < p_stream_processor->source_quantity; i++)
-    {
-        stream_processor_node *p_node = p_stream_processor->_p_sources[i];
-        parallel_thread *p_parallel_thread = (void *) 0;
-
-        switch(p_node->_type)
-        {
-            case STERAM_PROCESSOR_NODE_TYPE_INPUT_STREAM:
-                
-                parallel_thread_start(&p_parallel_thread, (fn_parallel_task *)stream_processor_input_stream_thread, (void *) p_node);
-
-                break;
-        }
-    }
-
-    for (size_t i = 0; i < p_stream_processor->drain_quantity; i++)
-    {
-        stream_processor_node *p_node = p_stream_processor->_p_drains[i];
-        parallel_thread *p_parallel_thread = (void *) 0;
-
-        switch(p_node->_type)
-        {
-            case STERAM_PROCESSOR_NODE_TYPE_OUTPUT_STREAM:
-                
-                parallel_thread_start(&p_parallel_thread, (fn_parallel_task *)stream_processor_output_stream_thread, (void *) p_node);
-
-                break;
-        }
-    }
-
-    //while(p_stream_processor->_p_sources[0]->input_stream.eof == false)
-    {
-        monitor_notify_all(&p_stream_processor->_p_sources[0]->_read_monitor);
-    }
-    while(p_stream_processor->_p_sources[0]->input_stream.eof == false);
-
-    // Success
-    return 1;
-}
-
-int stream_processor_task_add ( const char *p_name, fn_stream_processor_task *pfn_stream_processor_task )
-{
-    dict_add(p_stream_processor_tasks, p_name, pfn_stream_processor_task);
-    return 1;
-}
-
-int stream_processor_node_constructor ( stream_processor_node **pp_node, const char *p_name, json_value *p_value )
-{
-
-    // Initialized data
-    stream_processor_node *p_node = (void *) 0;
-    dict *p_dict = p_value->object;
-    json_value *p_type_value = dict_get(p_dict, "type");
-    fn_stream_processor_node_constructor *pfn_node_constructor = dict_get(p_stream_processor_node_constructors, p_type_value->string);
-    
-    // Strategy
-    pfn_node_constructor(&p_node, p_value);
-
-    strncpy(p_node->_name, p_name, strlen(p_name));
-
-    // Return a pointer to the caller
-    *pp_node = p_node;
-
-    // Success
-    return 1;
-}
-
-int stream_processor_node_input_stream_constructor ( stream_processor_node **pp_node, json_value *p_value )
-{
-
-    // Initialized data
-    stream_processor_node *p_node = realloc(0, sizeof(stream_processor_node));
-    dict *p_dict = p_value->object;
-    json_value *p_path_value = dict_get(p_dict, "path");
-    const char *const p_path = p_path_value->string;
-
-    // Open the input stream
-    p_node->input_stream.p_f = fopen(p_path, "r");
-    monitor_create(&p_node->_read_monitor);
-
-    // Copy the input stream path
-    strncpy(&p_node->input_stream._path, p_path, strlen(p_path));
-
-    // Store the node type
-    p_node->_type = STERAM_PROCESSOR_NODE_TYPE_INPUT_STREAM;
-
-    // Return a pointer to the caller
-    *pp_node = p_node;
-
-    // Success
-    return 1;
-}
-
-int stream_processor_node_output_stream_constructor ( stream_processor_node **pp_node, json_value *p_value )
-{
-
-    // Initialized data
-    stream_processor_node *p_node = realloc(0, sizeof(stream_processor_node));
-    dict *p_dict = p_value->object;
-    json_value *p_path_value = dict_get(p_dict, "path");
-    const char *const p_path = p_path_value->string;
-
-    // Open the output stream
-    p_node->output_stream.p_f = fopen(p_path, "w+");
-    monitor_create(&p_node->_read_monitor);
-
-    // Copy the output stream path
-    strncpy(&p_node->output_stream._path, p_path, strlen(p_path));
-
-    // Store the node type
-    p_node->_type = STERAM_PROCESSOR_NODE_TYPE_OUTPUT_STREAM;
-
-    // Return a pointer to the caller
-    *pp_node = p_node;
-
-    // Success
-    return 1;
-}
-
-int stream_processor_node_program_constructor ( stream_processor_node **pp_node, json_value *p_value )
-{
-    
-    // Initialized data
-    stream_processor_node *p_node = realloc(0, sizeof(stream_processor_node));
-    dict *p_dict = p_value->object;
-    json_value *p_task_value = dict_get(p_dict, "task");
-    const char *const p_task = p_task_value->string;
-
-    monitor_create(&p_node->_read_monitor);
-
-    // Store the node type
-    p_node->_type = STERAM_PROCESSOR_NODE_TYPE_PROGRAM;
-    
-    // Return a pointer to the caller
-    *pp_node = p_node;
 
     // Success
     return 1;
