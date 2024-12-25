c403db3c7488cba2f71338f5022dedf3f4d5e573
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-12-25 11:52:24 -0700

    [my module] Updated example module

--- my_module.c
+++ my_module.c
@@ -1,47 +1,50 @@
 #include <stdio.h>
 #include <json/json.h>
-#include <eddy/eddy.h>
+#include <list_processor/list_processor.h>
 
-json_value *my_module_a ( array *p_array );
-json_value *my_module_b ( array *p_array );
-json_value *my_module_c ( array *p_array );
+int my_module_a (list_processor *p_list_processor, array *p_in, json_value **p_out);
+int my_module_b (list_processor *p_list_processor, array *p_in, json_value **p_out);
+int my_module_c (list_processor *p_list_processor, array *p_in, json_value **p_out);
+
+// Data
+symbolic_expression_property _manifest[] =
+{
+    { .pfn_lp_eval = my_module_a, ._name = "alice" },
+    { .pfn_lp_eval = my_module_b, ._name = "bob" },
+    { .pfn_lp_eval = my_module_c, ._name = "charlie" }
+};
+size_t manifest_size = (sizeof(_manifest)/sizeof(symbolic_expression_property));
 
 void my_module_init ( void ) __attribute__((constructor));
 
 void my_module_init ( void ) 
 {
-
-    // Initialize eddy
-    eddy_init();
-
+    
     // My Module
-    eddy_register("my_module_alice"  , my_module_a);
-    eddy_register("my_module_bob"    , my_module_b);
-    eddy_register("my_module_charlie", my_module_c);
-
-    log_info("[my module] Registered\033[0m"); printf("\n");
+    for (size_t i = 0; i < manifest_size; i++)
+        lp_register(_manifest[i]._name, _manifest[i].pfn_lp_eval);
 
     // Done
     return;
 }
 
-json_value *my_module_a ( array *p_array )
+int my_module_a (list_processor *p_list_processor, array *p_in, json_value **p_out)
 {
     json_value *p_value = realloc(0, sizeof(json_value));
-    *p_value = (json_value) {.type = JSON_VALUE_STRING, .string = "Alice"};
-    return p_value;
+    *p_value = (json_value) {.type = JSON_VALUE_STRING, .string = "Alice"}, *p_out = p_value;
+    return 1;
 }
 
-json_value *my_module_b ( array *p_array )
+int my_module_b (list_processor *p_list_processor, array *p_in, json_value **p_out)
 {
     json_value *p_value = realloc(0, sizeof(json_value));
-    *p_value = (json_value) {.type = JSON_VALUE_STRING, .string = "Bob"};
-    return p_value;
+    *p_value = (json_value) {.type = JSON_VALUE_STRING, .string = "Bob"}, *p_out = p_value;
+    return 1;
 }
 
-json_value *my_module_c ( array *p_array )
+int my_module_c (list_processor *p_list_processor, array *p_in, json_value **p_out)
 {
     json_value *p_value = realloc(0, sizeof(json_value));
-    *p_value = (json_value) {.type = JSON_VALUE_STRING, .string = "Charlie"};
-    return p_value;
+    *p_value = (json_value) {.type = JSON_VALUE_STRING, .string = "Charlie"}, *p_out = p_value;
+    return 1;
 }
